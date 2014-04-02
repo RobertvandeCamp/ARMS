f34e4e7da2b26a2abe20a4c6eb29e485883a6d5b
Robert van de Camp <robertvandecamp@gmail.com>
2014-04-02 14:59:33 +0200

    Interfaces for risk calculator

--- src/main/com/adaptable/riskmanagement/RiskCalculator.java
+++ src/main/com/adaptable/riskmanagement/RiskCalculator.java
@@ -1,4 +1,11 @@
 package com.adaptable.riskmanagement;
 
 public class RiskCalculator {
+
+    public void calculateRisk() {
+        //Call rest client to get aggregate data
+        //Transform from json to java
+        //do calculation
+        //generate report
+    }
 }
